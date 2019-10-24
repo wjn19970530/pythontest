e777d978440e4f5e0c929538be3dae2064f8b208
tanglulu <25360880@qq.com>
2019-10-24 11:38:32 +0800

    钉钉通知添加teststeps数据

--- get_message.py
+++ get_message.py
@@ -14,12 +14,18 @@ def get_message():
     totalNum = data["stat"]["testcases"]["total"]
     successNum = data["stat"]["testcases"]["success"]
     failNum = data["stat"]["testcases"]["fail"]
+    steps_totalNum = data["stat"]["teststeps"]["total"]
+    steps_failNum = data["stat"]["teststeps"]["failures"]
+    steps_errorNum = data["stat"]["teststeps"]["errors"]
+    steps_skipNum = data["stat"]["teststeps"]["skipped"]
+    steps_successNum = steps_totalNum - steps_failNum - steps_errorNum - steps_skipNum
     if status == 0:
         status = 'Fail'
     if status == 1:
         status = 'Success'
-    message = " 【自动化执行结果】-  RESULT: %s  -  TOTAL: %s  -  SUCCESS: %s  -  FAIL: %s" \
-              % (status, totalNum, successNum, failNum)
+    message = "【自动化执行结果】=  RESULT: %s  =  TOTAL: %s  =  SUCCESS: %s  =  FAIL: %s  =  TESTSTEPS：total-%s  success-%s  " \
+              "fail-%s  error-%s" % (status, totalNum, successNum, failNum, steps_totalNum, steps_successNum,
+                                     steps_failNum, steps_errorNum)
     return message
 
 
