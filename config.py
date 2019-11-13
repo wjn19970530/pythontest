e0a3b9ce94dd194a6d0b4aa9b67a86463a5160c4
tanglulu <25360880@qq.com>
2019-11-13 15:08:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 usercenter = "autotest-usercenter_1001"
 mall = "autotest-mall_1001"
 uaa_authority = "autotest-uaa-authority_1001"
+pay = "pay_platform_auto-test_"
 
 # 从添加客户到交易完成链路配置车辆名称、车型全称
 carBrandName = "AutoTestCarBrand"
@@ -10,5 +11,5 @@ carFullName = "AutoTestCarBrandA1S"
 # 新建车辆配置系列名称，品牌使用交易链路的配置
 # 类型根据用例名称自定义写在用例中（接口中的类型名称参数化）
 carSeriesName = "A"
-
+carTypeName = "1S"
 
