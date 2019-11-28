594fb91805096d47657e5a1c64e17aff1be5adb8
tanglulu <25360880@qq.com>
2019-11-28 21:33:19 +0800

    测试结果保存至数据库

--- common/BaseCommon.py
+++ common/BaseCommon.py
@@ -54,6 +54,11 @@ class BaseCommon(object):
         step_expectedFailures = summary['stat']['teststeps']['expectedFailures']
         step_unexpectedSuccesses = summary['stat']['teststeps']['unexpectedSuccesses']
         step_successes = summary['stat']['teststeps']['successes']
+        # result = {'time': {'start_at': start_at, 'duration': duration}, 'success': status,
+        #           'stat': {'testcases': {'total': total, 'success': success, 'fail': fail},
+        #                    'teststeps': {'total': step_total, 'failures': step_failures, 'errors': step_errors,
+        #                                  'successes': step_successes, 'expectedFailures': step_expectedFailures,
+        #                                  'skipped': step_skipped, 'unexpectedSuccesses': step_unexpectedSuccesses}}}
         result = {'time': {'start_at': start_at, 'duration': duration}, 'success': status,
                   'stat': {'testcases': {'total': total, 'success': success, 'fail': fail},
                            'teststeps': {'total': step_total, 'failures': step_failures, 'errors': step_errors,
@@ -63,7 +68,8 @@ class BaseCommon(object):
 
     @staticmethod
     def save_msg_to_database(db, table, time, type, method):
-        sql = "insert into "+table+"(spend_time,type,method) values(" + str(time) + "," + str(type) + ","+str(method)+")"
+        sql = "insert into " + table + "(spend_time,type,method) values(" + str(time) + "," + str(type) + "," + str(
+            method) + ")"
         DBOperate(db).execute_sql(sql)
 
     @staticmethod
@@ -75,3 +81,27 @@ class BaseCommon(object):
                 if key in line:
                     value = (line.split('='))[1]
         return value
+
+    @staticmethod
+    def save_report_to_database(db, table, data, environment):
+        if data['success'] is True:
+            success = "true"
+        else:
+            success = "false"
+        field = "environment,result,case_total,case_success,case_fail,step_total,step_success,step_fail,step_error," \
+                "step_skip,step_expectedFailures,step_unexpectedSuccesses"
+        totalNum = str(data["stat"]["testcases"]["total"])
+        successNum = str(data["stat"]["testcases"]["success"])
+        failNum = str(data["stat"]["testcases"]["fail"])
+        steps_totalNum = str(data["stat"]["teststeps"]["total"])
+        steps_failNum = str(data["stat"]["teststeps"]["failures"])
+        steps_errorNum = str(data["stat"]["teststeps"]["errors"])
+        steps_skipNum = str(data["stat"]["teststeps"]["skipped"])
+        steps_successNum = str(data["stat"]["teststeps"]["successes"])
+        steps_expectedFailuresNum = str(data["stat"]["teststeps"]["expectedFailures"])
+        steps_unexpectedSuccessesNum = str(data["stat"]["teststeps"]["unexpectedSuccesses"])
+        value = "'"+environment+"','"+success+"','"+totalNum+"','"+successNum+"','"+failNum+"','"+steps_totalNum+"','"\
+                +steps_successNum+"','"+steps_failNum+"','"+steps_errorNum+"','"+steps_skipNum+"','"\
+                +steps_expectedFailuresNum+"','"+steps_unexpectedSuccessesNum+"'"
+        sql = "insert into "+table+"("+field+") values("+value+")"
+        DBOperate(db).execute_sql(sql)
